38bea1e4dd66cf0b84ca44f9900531c4244cb25a
RommelVz <Rommel.vz@hotmail.com>
2023-12-04 21:37:05 -0500

    actualizacion dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,7 +8,9 @@ use App\Models\Compra;
 use App\Models\Inventario;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+use DateTime;
+use DateInterval;
+use DatePeriod;
 class DashboardController extends Controller
 {
     public function info(){
@@ -26,15 +28,41 @@ class DashboardController extends Controller
     }
     public function VentasMensual()
     {
+        $nombresMeses = [
+            'ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV', 'DIC'
+        ];
+
+        $ventasMensuales = [];
+        foreach ($nombresMeses as $mes) {
+            $ventasMensuales[] = [
+                'mes' => sprintf('%02d', array_search($mes, $nombresMeses) + 1) . '.' . $mes,
+                'total' => 0,
+                'año' => date('Y'),
+            ];
+        }
         $ventas = Venta::select(
             DB::raw('SUM(total) as total'),
-            DB::raw("CONCAT(LPAD(DATE_FORMAT(created_at, '%m'), 2, '0'), '-', DATE_FORMAT(created_at, '%b')) as mes"),
-            DB::raw("DATE_FORMAT(created_at, '%Y') as año")
-        )->where('estado', 1)
-        ->groupBy('mes', 'año')
-        ->get();
-            return $ventas;
+            DB::raw('DATE_FORMAT(created_at, "%m") as numero_mes'),
+            DB::raw('DATE_FORMAT(created_at, "%Y") as año')
+        )
+            ->where('estado', 1)
+            ->groupBy('numero_mes', 'año')
+            ->get();
+
+        foreach ($ventas as $venta) {
+            $numeroMes = intval($venta->numero_mes);
+            $ventasMensuales[$numeroMes - 1] = [
+                'mes' => sprintf('%02d', $numeroMes) . '.' . $nombresMeses[$numeroMes - 1],
+                'total' => $venta->total,
+                'año' => $venta->año,
+            ];
+        }
+        return $ventasMensuales;
     }
+
+
+
+
     public function ComprasMensual()
     {
         $compras = Compra::select(
@@ -68,26 +96,44 @@ class DashboardController extends Controller
 
         return $compras;
     }
-    public function PromedioInventario()
-    {
-        $inventario = Inventario::select(
-            DB::raw('DATE_FORMAT(created_at, "%Y-%m-%d") as fecha'),
-            DB::raw('SUM(CASE WHEN tipo = 1 THEN cantidad ELSE 0 END) as ingresos'),
-            DB::raw('SUM(CASE WHEN tipo = 2 THEN cantidad ELSE 0 END) as egresos')
-        )
-            ->where('estado', 1)
-            ->groupBy('fecha')
-            ->get();
+public function PromedioInventario()
+{
+    $nombresMeses = [
+        'ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV', 'DIC'
+    ];
+    $nombresNumero = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+    ];
+    // Obtener los datos de inventario
+    $inventario = Inventario::select(
+        DB::raw('DATE_FORMAT(created_at, "%Y-%m-%d") as fecha'),
+        DB::raw('SUM(CASE WHEN tipo = 1 THEN cantidad ELSE 0 END) as ingresos'),
+        DB::raw('SUM(CASE WHEN tipo = 2 THEN cantidad ELSE 0 END) as egresos')
+    )
+        ->where('estado', 1)
+        ->groupBy('fecha')
+        ->get();
 
-        // Ahora que tenemos los ingresos y egresos por fecha, calculamos el promedio de inventario.
-        $promedioInventario = $inventario->map(function ($item) {
-            return [
-                'fecha' => $item->fecha,
-                'promedio_inventario' => ($item->ingresos + $item->egresos) / 2,
-            ];
+    $promedioPorMes = [];
+
+    foreach ($nombresNumero as $mes) {
+        $inventarioMes = $inventario->filter(function ($item) use ($mes) {
+            $numeroMes = date('m', strtotime($item->fecha));
+
+            return $numeroMes == $mes;
         });
 
-        return $promedioInventario;
+        $promedio = $inventarioMes->isEmpty() ? 0 : $inventarioMes->avg(function ($item) {
+            return ($item->ingresos + $item->egresos) / 2;
+        });
+
+        $promedioPorMes[] = [
+            'fecha' => sprintf('%02d', $mes) . '.' . $nombresMeses[$mes - 1], // Formato '01.ene'
+            'promedio_inventario' => $promedio,
+        ];
     }
+
+    return $promedioPorMes;
+}
 
 }
